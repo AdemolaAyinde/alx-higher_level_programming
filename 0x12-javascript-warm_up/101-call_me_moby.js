0586863115d5a05a4fa661c544766f489990abed
Ademola Ayinde <122974487+AdemolaAyinde@users.noreply.github.com>
2023-08-16 10:42:35 +0100

    Update 101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction()
-  }
-}
-module.exports = callMeMoby
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) theFunction();
+};
