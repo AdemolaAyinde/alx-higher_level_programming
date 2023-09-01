e096c278ddc4860e12cdf1c78e5292cc8fd7ea96
Ademola Ayinde <122974487+AdemolaAyinde@users.noreply.github.com>
2023-09-01 12:09:34 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""A  script that:
-    - takes in a URL and an email address
-    - sends a POST request to the passed URL with the email as a parameter
-    - displays the body of the response.
-    """
-    import sys
-    import requests
+"""script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter, and finally displays
+the body of the response.
+"""
+import requests
+import sys
 
-
-    if _name_ == "_main_":
-            url = sys.argv[1]
-                value = {"email": sys.argv[2]}
-
-                    r = requests.post(url, data=value)
-                        print(r.text)
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    r = requests.post(url, data={'email': email})
+    print(r.text)
