098ebf3de19bfb1dc42b31e6dd74381d8da85bc6
Ademola Ayinde <122974487+AdemolaAyinde@users.noreply.github.com>
2023-09-01 12:08:59 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""A script that
-- takes in a URL
-- sends a request to the URL
-- displays the body of the response.
-"""
+""" script that takes in a URL, sends a request to
+ the URL and displays the body of the response."""
 import sys
+
 import requests
 
-
-if _name_ == "_main_":
-        url = sys.argv[1]
-
-            r = requests.get(url)
-                if r.status_code >= 400:
-                            print("Error code: {}".format(r.status_code))
-                                else:
-                                            print(r.text)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
